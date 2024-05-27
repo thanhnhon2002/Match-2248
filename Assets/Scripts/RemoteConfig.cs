a7deb84017f469c10db4aacdacd8543e5fd46df1
Bình <kaisne0708@gmail.com>
2024-05-27 14:53:50 +0700

    Min session time show ads

--- Assets/Scripts/RemoteConfig.cs
+++ Assets/Scripts/RemoteConfig.cs
@@ -10,10 +10,12 @@ public class RemoteConfig : MonoBehaviour
     [SerializeField] private bool _collapsibleBannerFallbackEnabled = true;
     [SerializeField] private float _collapsibleBannerInterval = 40f;
     [SerializeField] private float _timeBetweenAds = 120f;
+    [SerializeField] private float _minSessionTimeShowAds = 60f;
     public bool COLLAPSIBLE_BANNER_ENABLED => _collapsibleBannerEnabled;
     public bool COLLAPSIBLE_FALLBACK_ENABLED => _collapsibleBannerFallbackEnabled;
     public float COLLAPSIBLE_BANNER_INTERVAL => _collapsibleBannerInterval;
     public float TIME_BETWEEN_ADS => _timeBetweenAds;
+    public float MIN_SESSION_TIME_SHOW_ADS => _minSessionTimeShowAds;
 
     public void InitializeRemoteConfig()
     {
@@ -23,6 +25,7 @@ public class RemoteConfig : MonoBehaviour
             { "collapsible_banner_fallback_enabled", _collapsibleBannerFallbackEnabled},
             { "collapsible_banner_interval", _collapsibleBannerInterval},
             { "show_interstitial_ads_interval", _timeBetweenAds},
+            { "min_session_time_show_ads", _minSessionTimeShowAds}
         };
         FirebaseRemoteConfig.DefaultInstance.SetDefaultsAsync(defaults).ContinueWithOnMainThread(task =>
         {
@@ -45,17 +48,20 @@ public class RemoteConfig : MonoBehaviour
                 var currentBannerFallbackEnabled = FirebaseRemoteConfig.DefaultInstance.GetValue("collapsible_banner_fallback_enabled").BooleanValue;
                 var currentBannerInterval = FirebaseRemoteConfig.DefaultInstance.GetValue("collapsible_banner_interval").DoubleValue;
                 var currentInterval = FirebaseRemoteConfig.DefaultInstance.GetValue("show_interstitial_ads_interval").DoubleValue;
+                var currentMinSessionTimeShowAds = FirebaseRemoteConfig.DefaultInstance.GetValue("min_session_time_show_ads").DoubleValue;
                 if (_showDebug)
                 {
                     Debug.Log("Current banner enabled: " + currentBannerEnabled);
                     Debug.Log("Current banner fallback enabled: " + currentBannerFallbackEnabled);
                     Debug.Log("Current banner interval: " + currentBannerInterval);
                     Debug.Log("Current interstitial ads interval: " + currentInterval);
+                    Debug.Log("Current min session time show ads: " + currentMinSessionTimeShowAds);
                 }
                 _collapsibleBannerEnabled = currentBannerEnabled;
                 _collapsibleBannerFallbackEnabled = currentBannerFallbackEnabled;
                 _collapsibleBannerInterval = (float)currentBannerInterval;
                 _timeBetweenAds = (float)currentInterval;
+                _minSessionTimeShowAds = (float)currentMinSessionTimeShowAds;
             }
             else
             {
