d3639972671e761671cff21f8133094198cf6cab
Phat Tan <phattan229@gmail.com>
2024-06-14 16:53:49 +0700

    some fix

--- Assets/Scripts/Cell/Cell.cs
+++ Assets/Scripts/Cell/Cell.cs
@@ -138,8 +138,8 @@ public class Cell : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHand
         spriteRenderer.DOColor(CellColor.Instance.GetCellColor(endValue), Const.DEFAULT_TWEEN_TIME);
         LeanTween.value(1000, 9999, Const.DEFAULT_TWEEN_TIME).setOnUpdate((float x) =>
         {
-            valueTxt.text = x.ToString().Substring(0,4);
-        }).setOnComplete(() => Value = endValue);
+            valueTxt.text = x.ToString().Substring(0, 4);
+        });
     }
 
 #if UNITY_EDITOR
