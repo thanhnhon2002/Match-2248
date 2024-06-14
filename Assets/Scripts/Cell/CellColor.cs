34ad1fd37a0bad5cdbbff0ce5d7bb8bf00eb248e
duchuy <duchuy.huynh.413@gmail.com>
2024-06-14 15:39:22 +0700

    update firebase 12.0 + fix mytarget

--- Assets/Scripts/Cell/CellColor.cs
+++ Assets/Scripts/Cell/CellColor.cs
@@ -7,20 +7,20 @@ public class CellColor : MonoBehaviour
 {
     public static CellColor Instance { get; private set; }
     [SerializeField] private Color[] colors;
-    private void Awake ()
+    private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad (gameObject);
+            DontDestroyOnLoad(gameObject);
         }
-        else Destroy (gameObject);
+        else Destroy(gameObject);
     }
 
-    public Color GetCellColor (BigInteger value)
+    public Color GetCellColor(BigInteger value)
     {
         Mathf mathf;
-        var pow = mathf.LogBigInt (value, 2);
+        var pow = mathf.LogBigInt(value, 2);
         var index = pow % colors.Length;
         var color = colors[index];
         color.a = 1f;
