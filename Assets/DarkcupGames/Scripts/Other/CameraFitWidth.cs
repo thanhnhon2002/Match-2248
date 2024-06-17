653eba7a8a6e5eb8637c80b02ace5d37e9a45fbc
ReturnThis <nhon91ddt@gmail.com>
2024-06-17 11:53:43 +0700

    fix scene

--- Assets/DarkcupGames/Scripts/Other/CameraFitWidth.cs
+++ Assets/DarkcupGames/Scripts/Other/CameraFitWidth.cs
@@ -7,10 +7,11 @@ namespace DarkcupGames
     public class CameraFitWidth : MonoBehaviour
     {
         public float ratio = 1f;
-
-        void Start()
+#if UNITY_EDITOR 
+        void Update()
         {
             Camera.main.orthographicSize = ratio * Screen.height / Screen.width;
         }
+#endif
     }
 }
