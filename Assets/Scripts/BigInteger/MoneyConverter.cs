81eaa2439e9c0f6e81eee500a5684afec0742f16
ReturnThis <nhon91ddt@gmail.com>
2024-06-19 11:46:46 +0700

    update code moneyconvert

--- Assets/Scripts/BigInteger/MoneyConverter.cs
+++ Assets/Scripts/BigInteger/MoneyConverter.cs
@@ -15,4 +15,19 @@ public static class MoneyConveter
         else if (value < 10000000000) return (value / 1000000000).ToString() + "," + (value / 100000000 % 10) + "B";
             else return (value / 1000000000).ToString() + "B";
     }
+    public static string ConverNameValueBestCode(long value)
+    {
+        if (value < 1000) return value.ToString();
+        for(int i=3;i>0;i--)
+        {
+            if (value / (long)Math.Pow(10, i*3) > 0) return Convert(i,value);
+        }
+        return "";
+    }
+    static string Convert(int i, long value)
+    {
+        string extension = new NameExtension(i).name;
+        if (value / (long)Math.Pow(10, i * 3 + 1) == 0) return (value / (long)Math.Pow(10, i * 3)).ToString() + "," + (value / (long)Math.Pow(10, i * 3-1) % 10) + extension;
+        else return (value / (long)Math.Pow(10, i * 3)).ToString() + extension;
+    }
 }
