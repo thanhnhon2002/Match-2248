217d6a7f1abfb7422ed0344548b1f1035af26ad2
duchuy <duchuy.huynh.413@gmail.com>
2024-07-11 12:09:45 +0700

    anim combo manager

---
+++ Assets/Scripts/Ann Animation Pack/AnimComboManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// this class will take index of animComboList to become an ID of each gameobject popup. 
+/// If the popup, popup incorrectly, try to sort them in hierarchy then run Function "Update animComboList" again
+/// </summary>
+public class AnimComboManager : MonoBehaviour
+{
+    
+}
